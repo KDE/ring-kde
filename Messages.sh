7faa0435520e7e76720b6f332367be3060201de0
Montel Laurent <montel@kde.org>
2013-02-12 13:12:31 +0100

    Minor

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,3 @@
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/sflphone-client-kde.pot
+rm -rf rc.cpp
