4b01c5ced209b9643b49ea539755b56872fc5934
Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com>
2013-06-28 17:02:16 -0400

    [ #26396 ] Export role names for HistoryModel

--- src/lib/historymodel.cpp
+++ src/lib/historymodel.cpp
@@ -116,6 +116,35 @@ HistoryModel::HistoryModel():QAbstractItemModel(QCoreApplication::instance()),m_
    m_spInstance = this;
    reloadCategories();
    m_lMimes << MIME_PLAIN_TEXT << MIME_PHONENUMBER << MIME_HISTORYID;
+   QHash<int, QByteArray> roles = roleNames();
+   roles.insert(Call::Role::Name          ,QByteArray("name"));
+   roles.insert(Call::Role::Number        ,QByteArray("number"));
+   roles.insert(Call::Role::Direction     ,QByteArray("direction"));
+   roles.insert(Call::Role::Date          ,QByteArray("date"));
+   roles.insert(Call::Role::Length        ,QByteArray("length"));
+   roles.insert(Call::Role::FormattedDate ,QByteArray("formattedDate"));
+   roles.insert(Call::Role::HasRecording  ,QByteArray("hasRecording"));
+   roles.insert(Call::Role::HistoryState  ,QByteArray("historyState"));
+   roles.insert(Call::Role::Filter        ,QByteArray("filter"));
+   roles.insert(Call::Role::FuzzyDate     ,QByteArray("fuzzyDate"));
+   roles.insert(Call::Role::IsBookmark    ,QByteArray("isBookmark"));
+   roles.insert(Call::Role::Security      ,QByteArray("security"));
+   roles.insert(Call::Role::Department    ,QByteArray("department"));
+   roles.insert(Call::Role::Email         ,QByteArray("email"));
+   roles.insert(Call::Role::Organisation  ,QByteArray("organisation"));
+   roles.insert(Call::Role::Codec         ,QByteArray("codec"));
+   roles.insert(Call::Role::IsConference  ,QByteArray("isConference"));
+   roles.insert(Call::Role::Object        ,QByteArray("object"));
+   roles.insert(Call::Role::PhotoPtr      ,QByteArray("photoPtr"));
+   roles.insert(Call::Role::CallState     ,QByteArray("callState"));
+   roles.insert(Call::Role::Id            ,QByteArray("id"));
+   roles.insert(Call::Role::StartTime     ,QByteArray("startTime"));
+   roles.insert(Call::Role::StopTime      ,QByteArray("stopTime"));
+   roles.insert(Call::Role::DropState     ,QByteArray("dropState"));
+   roles.insert(Call::Role::DTMFAnimState ,QByteArray("dTMFAnimState"));
+   roles.insert(Call::Role::LastDTMFidx   ,QByteArray("lastDTMFidx"));
+   roles.insert(Call::Role::IsRecording   ,QByteArray("isRecording"));
+   setRoleNames(roles);
 } //initHistory
 
 ///Destructor
