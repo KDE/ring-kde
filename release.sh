6916aa6e121d2a85fe4d2457c214f48407d7cec5
Emmanuel Lepage Vallee <elv1313@gmail.com>
2014-07-10 16:44:59 -0400

    Rename sflphone-client-kde to sflphone-kde

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ echo 'Make sure config.ini  and all code version refewrences are ready [PRESS EN
 read
 mkdir -p build
 cd build
-git clone git://anongit.kde.org/kde-dev-scripts
+git clone http://anongit.kde.org/kde-dev-scripts
 cp ../data/config.ini ./
 cp kde-dev-scripts/createtarball/create_tarball.rb ./
-ruby create_tarball.rb -n -a sflphone-client-kde && mv sflphone-client-kde-*.tar.* ../
+ruby create_tarball.rb -n -a sflphone-kde && mv sflphone-kde-*.tar.* ../
