0f52f5849137bc2d37a52ea2a659007d23ed470c
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-03-22 02:45:51 -0400

    build: Auto-fetch the Ring.cx dependencies
    Download the required XML files and Ring-LRC. This will simplify
    building Ring-KDE. It will also make it easier to test in the KDE
    CI.

---
+++ cmake/FetchRing-LRC.cmake
@@ -0,0 +1,80 @@
+# In case dring and Ring-LRC are not found, fetch them from git and curl to
+# setup a minimal environment capable of building Ring-KDE. It also enable
+# static libraries to isolate Ring-KDE from any potential LRC API break.
+#
+# Ring-GNOME also enable static libraries for the same reason. This module
+# doesn't produce a "ready to use" binary. The daemon is still a separate
+# process. Single binary Ring isn't officially supported on Linux and sending
+# patches to keep it working upstream will only annoy its maintainer before
+# being ignored.
+
+CMAKE_MINIMUM_REQUIRED(VERSION 3.0)
+
+find_package(Git REQUIRED)
+include(ExternalProject)
+
+# Fetch the XMLs
+set(XML_PATH ${CMAKE_CURRENT_BINARY_DIR}/xml/)
+
+if (NOT EXISTS ${XML_PATH}/cx.ring.Ring.CallManager.xml)
+
+    message(STATUS "Downloading the GNU Ring API definition")
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.CallManager.xml
+        ${XML_PATH}cx.ring.Ring.CallManager.xml INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.ConfigurationManager.xml
+        ${XML_PATH}cx.ring.Ring.ConfigurationManager.xml INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.Instance.xml
+        ${XML_PATH}cx.ring.Ring.Instance.xml INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.PresenceManager.xml
+        ${XML_PATH}cx.ring.Ring.PresenceManager.xml INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.VideoManager.xml
+        ${XML_PATH}cx.ring.Ring.VideoManager.xml INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/bin/dbus/cx.ring.Ring.VideoManager.xml
+        ${XML_PATH}cx.ring.Ring.VideoManager.xml INACTIVITY_TIMEOUT 30)
+
+    # Fetch the constants
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/src/dring/account_const.h
+        ${CMAKE_CURRENT_BINARY_DIR}/dring/account_const.h INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/src/dring/security_const.h
+        ${CMAKE_CURRENT_BINARY_DIR}/dring/security_const.h INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/src/dring/media_const.h
+        ${CMAKE_CURRENT_BINARY_DIR}/dring/media_const.h INACTIVITY_TIMEOUT 30)
+
+    file(DOWNLOAD https://raw.githubusercontent.com/savoirfairelinux/ring-daemon/master/src/dring/call_const.h
+        ${CMAKE_CURRENT_BINARY_DIR}/dring/call_const.h INACTIVITY_TIMEOUT 30)
+endif()
+
+message(STATUS "Fetching Ring-LRC from GitHub")
+
+if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/ring-lrc)
+    execute_process(COMMAND
+        git clone https://github.com/Elv13/ring-lrc.git --branch work5 ${CMAKE_CURRENT_BINARY_DIR}/ring-lrc
+    )
+else()
+    execute_process(COMMAND
+        /bin/sh -c "cd ${CMAKE_CURRENT_BINARY_DIR}/ring-lrc && git fetch origin && git reset --hard origin/work5"
+    )
+endif()
+
+# Build Ring-LRC
+SET(RING_XML_INTERFACES_DIR ${XML_PATH})
+
+get_filename_component(BIN_DIR_NAME ${CMAKE_CURRENT_BINARY_DIR} NAME)
+
+SET(ring_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/dring/)
+
+LIST(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_BINARY_DIR}/ring-lrc/cmake/")
+
+ADD_SUBDIRECTORY(${CMAKE_CURRENT_BINARY_DIR}/ring-lrc/)
+
+SET(LibRingClient_DIR ${CMAKE_CURRENT_BINARY_DIR}/${BIN_DIR_NAME}/ring-lrc/)
+
+INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR}/ring-lrc/src)
